4d9951ed8918ea03aeef7470a90801796a48b44e
nofumay <nofumayy@gmail.com>
2025-03-18 22:05:16 -0300

    Adiciona classe principal do Spring Boot

---
+++ backend/src/main/java/com/damas/JogoDamasApplication.java
@@ -0,0 +1,12 @@
+package com.damas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoDamasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JogoDamasApplication.class, args);
+    }
+}
